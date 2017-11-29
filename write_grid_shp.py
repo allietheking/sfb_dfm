6977f6a64c3071a29665444e69d341a606ebd1ed
Rusty Holleman <rustyh@sfei.org>
2017-11-29 09:02:19 -0800

    Write cells, too

--- write_grid_shp.py
+++ write_grid_shp.py
@@ -13,3 +13,11 @@ g=dfm_grid.DFMGrid('sfei_v19_net.nc')
 ##
 
 g.write_edges_shp('derived/grid-edges.shp')
+
+##
+
+cell_depth=g.interp_node_to_cell(g.nodes['depth'])
+
+g.write_cells_shp('derived/grid-cells.shp',
+                  extra_fields=[ ('depth',cell_depth) ])
+
